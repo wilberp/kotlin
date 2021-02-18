8ec4bade7501c936bf95a9bdc804bc173606e3ef
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-02-18 20:23:49 -0300

    commit inicial

---
+++ fundamentos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,12 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "wils"
+    var cpf: String = "277"
+}
+
+fun main(){
+    val pessoa = Pessoa()
+    println(pessoa.nome)
+    println(pessoa.cpf)
+}
