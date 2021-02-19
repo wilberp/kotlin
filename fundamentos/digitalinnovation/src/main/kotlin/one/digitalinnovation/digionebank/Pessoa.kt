ec6384d1de0e2a29470a155f24fb178e877acbd9
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-02-18 22:14:45 -0300

    colocando privete set para deixar os campos com valores imutaveis

--- fundamentos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,10 +3,13 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "wils"
     var cpf: String = "277"
+    //faz com que os campos não seja mutaveis no caminho
+    private set
 }
 
 fun main(){
     val pessoa = Pessoa()
+//    pessoa.cpf = "1234"
     println(pessoa.nome)
     println(pessoa.cpf)
 }
