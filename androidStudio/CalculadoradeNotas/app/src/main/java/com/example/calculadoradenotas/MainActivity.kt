b935636925132e57fa39bb6e00c116f93dea7c62
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-03 23:28:55 -0300

    versao inicial Calculadora de Notas
    Estudo Android Studio

---
+++ androidStudio/CalculadoradeNotas/app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.calculadoradenotas
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btCalcular = calcular
+        val resultado = resultados
+
+        btCalcular.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1+nota2)/2
+            val faltas = Integer.parseInt((faltas.text.toString()))
+
+
+            if (media>=6 && faltas <=10){
+                resultado.setText("Aluno foi Aprovado" + "\n" + " Nota Final: " + media + " Faltas: " + faltas)
+                resultado.setTextColor(Color.GREEN)
+            }else {resultado.setText("Aluno foi Reprovado" + "\n" + " Nota Final: " + media + " Faltas: " + faltas)
+            resultado.setTextColor(Color.RED)}
+        }
+    }
+}
