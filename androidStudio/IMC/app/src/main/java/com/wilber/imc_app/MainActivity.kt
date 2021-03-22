f675baf12ce6e3409bc6adb39101a1a3d2e27355
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-22 17:25:05 -0300

    atualização tela IMC

--- androidStudio/IMC/app/src/main/java/com/wilber/imc_app/MainActivity.kt
+++ androidStudio/IMC/app/src/main/java/com/wilber/imc_app/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.wilber.imc_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+   private fun setListeners(){
+
+       alturaEDT?.doAfterTextChanged { text ->
+           Toast.makeText(this, text.toString(), Toast.LENGTH_LONG).show()
+       }
+
+       pesoEDT.doOnTextChanged{text, start, before, count ->   titleTXT?.text = text}
+
+
+       calcularBTN.setOnClickListener{
+
+           calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+       }
+   }
+
+    //private fun abrirNovaTelaComResultados(imc: String, faixa: String) {
+    private fun abrirNovaTelaComResultados(imc: String) {
+        val result = Intent(this, ResultActivity::class.java)
+        result.putExtra(ResultActivity.IMC_ID, imc)
+    //    result.putExtra(ResultActivity.FAIXA_DE_PESO, faixa)
+        startActivity(result)
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        var vimc = 0F
+
+        if (peso != null && altura != null) {
+            vimc = peso / (altura * altura)
+        //    titleTXT.text = "Seu IMC é \n %.2f".format(vimc)
+            abrirNovaTelaComResultados( "%.2f".format(vimc))
+        }else{
+
+        }
+
+
     }
 }
--- androidStudio/IMC/app/src/main/java/com/wilber/imc_app/ResultActivity.kt
+++ androidStudio/IMC/app/src/main/java/com/wilber/imc_app/ResultActivity.kt
@@ -1,11 +1,70 @@
 package com.wilber.imc_app
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.ColorInt
+import kotlinx.android.synthetic.main.activity_result.*
+import java.time.format.TextStyle
+import kotlin.text.toFloat as textToFloat
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        setListeners()
+    }
+    companion object{
+        const val FAIXA_DE_PESO = "Peso"
+        const val IMC_ID = "imc"
+    }
+
+    private fun setListeners(){
+        closeBTN?.setOnClickListener {
+            onBackPressed()
+            //retorna para a tela anterior
+        }
+
+        //exibe os valores passados pela tela anterior
+        resultTXT?.text = intent.getStringExtra(IMC_ID)
+        val vimc:String = intent.getStringExtra(IMC_ID).toString()
+        defineFaixaClassificacao(vimc)
+        //resultTextTXT?.text = intent.getStringExtra(FAIXA_DE_PESO)
+    }
+
+    private fun defineFaixaClassificacao(imc: String){
+
+        if(imc < "18,5") {
+            resultTextTXT?.text = "MAGREZA"
+            resultTextTXT?.textSize = 32F
+            resultTextTXT?.setTextColor(Color.CYAN)
+            resultTextTXT?.setBackgroundColor(Color.BLACK)
+        }
+        else if(imc in "18,5".."24,9") {
+            resultTextTXT?.text = "NORMAL"
+            resultTextTXT?.textSize = 32F
+            resultTextTXT?.setTextColor(Color.GREEN)
+            resultTextTXT?.setBackgroundColor(Color.BLACK)
+        }
+        else if(imc in "25,0".."29,9") {
+            resultTextTXT?.text = "SOBREPESO"
+            resultTextTXT?.textSize = 32F
+            resultTextTXT?.setTextColor(Color.YELLOW)
+            resultTextTXT?.setBackgroundColor(Color.BLACK)
+        }
+        else if(imc in "30,0".."39,9") {
+            resultTextTXT?.text = "OBESIDADE"
+            resultTextTXT?.textSize = 32F
+            resultTextTXT?.setTextColor(Color.RED)
+            resultTextTXT?.setBackgroundColor(Color.BLACK)
+        }
+        else{
+            resultTextTXT?.text = "OBESIDADE MORBIDA"
+            resultTextTXT?.textSize = 32F
+            resultTextTXT?.setTextColor(Color.MAGENTA)
+            resultTextTXT?.setBackgroundColor(Color.BLACK)
+        }
     }
 }
