d16eed94f7acf58ad5a56aa2c8aae397603d7622
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-22 21:57:08 -0300

    criacao de layout e metodo onCreateViewHolder

---
+++ androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/Contact.kt
@@ -0,0 +1,7 @@
+package br.com.wilber.bootcampkotlin
+
+data class Contact(
+    var name: String,
+    var phone: String,
+    var photograph: String
+)
---
+++ androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/ContactAdapter.kt
@@ -0,0 +1,28 @@
+package br.com.wilber.bootcampkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
+   private val list: MutableList<Contact> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+        return ContactAdapterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ContactAdapterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(contact: Contact){
+        }
+    }
+}
