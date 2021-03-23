aa4074029faea69b43d296af10c5e4a05946a4ee
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-23 16:57:57 -0300

    options menu e navigation drawer

--- androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/MainActivity.kt
+++ androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/MainActivity.kt
@@ -2,9 +2,17 @@ package br.com.wilber.bootcampkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import br.com.wilber.bootcampkotlin.R.layout.activity_main
+import br.com.wilber.bootcampkotlin.R.layout.drawer_menu
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,13 +24,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_main)
+        setContentView(drawer_menu)
         bindViews()
         updateList()
     }
 
+    private fun initDrawer(){
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbars = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbars)
 
-    private fun bindViews(){
+        var toggle = ActionBarDrawerToggle(this, drawerLayout, toolbars, R.string.open_drawer, R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+   private fun bindViews(){
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
     }
@@ -42,6 +58,29 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         )
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 ->{
+                showToast("Exibindo item menu 1")
+            true
+        }
+            R.id.item_menu_2 ->{
+                showToast("Exibindo item menu 2")
+                true
+        }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
 
