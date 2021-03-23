8849c7b2289ef7c112ba3612be40ff7368088859
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-23 14:24:32 -0300

    recycleview e cardview

--- androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/ContactAdapter.kt
+++ androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/ContactAdapter.kt
@@ -3,6 +3,8 @@ package br.com.wilber.bootcampkotlin
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
@@ -21,8 +23,21 @@ class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHol
         return list.size
     }
 
+    fun updateList(list: List<Contact>){
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class ContactAdapterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val tvName: TextView = itemView.findViewById(R.id.tv_name)
+        private val tvPhone: TextView = itemView.findViewById(R.id.tv_phone)
+        private val ivPhotograph: ImageView = itemView.findViewById(R.id.iv_photograph)
+
         fun bind(contact: Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+
         }
     }
 }
--- androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/MainActivity.kt
+++ androidStudio/Bootcampkotlin/app/src/main/java/br/com/wilber/bootcampkotlin/MainActivity.kt
@@ -2,11 +2,46 @@ package br.com.wilber.bootcampkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.wilber.bootcampkotlin.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList:RecyclerView by lazy{
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        bindViews()
+        updateList()
+    }
+
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                "Wilian Peres",
+                "(16) 12345-43",
+                "img.png"
+                    ),
+                Contact(
+                "Wilian Peres",
+                "(16) 12345-43",
+                "img.png"
+                )
+            )
+        )
     }
 }
 
