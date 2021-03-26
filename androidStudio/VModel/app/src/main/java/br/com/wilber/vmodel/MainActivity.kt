304780e13796c028c184681b9fca8904e98ecd45
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-25 22:07:49 -0300

    ciclo de vida

--- androidStudio/VModel/app/src/main/java/br/com/wilber/vmodel/MainActivity.kt
+++ androidStudio/VModel/app/src/main/java/br/com/wilber/vmodel/MainActivity.kt
@@ -2,10 +2,86 @@ package br.com.wilber.vmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtContador: EditText
+    lateinit var btnDados : Button
+    lateinit var btnMostrar: Button
+    var contador : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logar(valor = "onCreate")
+
+        initDados()
+        initContador()
+        initClick()
+        validaContador()
+
+    }
+
+    //ciclo de vida
+    override fun onStart() {
+        logar(valor = "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logar(valor = "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        logar(valor = "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logar(valor = "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logar(valor = "onDestroy")
+        super.onDestroy()
+    }
+
+
+    private fun logar(tag: String = "Ciclo de vida", valor: String){
+        Log.d(tag, valor)
+    }
+
+    private fun validaContador() {
+        if(contador > 5){
+            contador = 0
+        }
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+        btnMostrar.setOnClickListener {
+            Toast.makeText(this,"Valor Contador ${contador.toString()}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initContador() {
+        txtContador.setText(contador.toString())
+    }
+
+    private fun initDados(){
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
     }
 }
