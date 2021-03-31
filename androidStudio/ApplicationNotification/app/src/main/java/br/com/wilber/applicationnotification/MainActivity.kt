b61bc4c5f34259a864e03b4c8f36ce8e04b76391
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-30 22:57:19 -0300

    notification push

--- androidStudio/ApplicationNotification/app/src/main/java/br/com/wilber/applicationnotification/MainActivity.kt
+++ androidStudio/ApplicationNotification/app/src/main/java/br/com/wilber/applicationnotification/MainActivity.kt
@@ -2,13 +2,13 @@ package br.com.wilber.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnSend: Button
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +16,9 @@ class MainActivity : AppCompatActivity() {
 
         btnSend = findViewById(R.id.button_send_notification)
         btnSend.setOnClickListener {
-
+            //essa é a localnotification
+            this.showNotification("1234", "bootcamp Android", "Kotlin Android Curso")
         }
+        Log.i("**newToken", FirebaseInstanceId.getInstance().token.toString())
     }
 }
