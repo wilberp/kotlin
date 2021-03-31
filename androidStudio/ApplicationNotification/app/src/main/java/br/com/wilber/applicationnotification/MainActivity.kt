d55e241e6efdc15c67bf2c7cc2dfc2e857019976
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-03-30 21:02:27 -0300

    versao inicial notifications

---
+++ androidStudio/ApplicationNotification/app/src/main/java/br/com/wilber/applicationnotification/MainActivity.kt
@@ -0,0 +1,22 @@
+package br.com.wilber.applicationnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+
+        }
+    }
+}
---
+++ androidStudio/ApplicationNotification/app/src/main/java/br/com/wilber/applicationnotification/NotificationUtils.kt
@@ -0,0 +1,18 @@
+package br.com.wilber.applicationnotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+
+lateinit var  notificationChannel: NotificationChannel
+lateinit var notificationManager: NotificationManager
+lateinit var builder: NotificationCompat.Builder
+
+    fun Context.showNotification(channelId: String, title: String, body: String){
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
