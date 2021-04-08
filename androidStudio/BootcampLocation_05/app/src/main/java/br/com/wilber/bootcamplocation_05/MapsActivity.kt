94b2ecca750c21e981a0371a6289e7c5a1200865
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-04-08 17:24:21 -0300

    final Location

--- androidStudio/BootcampLocation_05/app/src/main/java/br/com/wilber/bootcamplocation_05/MapsActivity.kt
+++ androidStudio/BootcampLocation_05/app/src/main/java/br/com/wilber/bootcamplocation_05/MapsActivity.kt
@@ -1,12 +1,15 @@
 package br.com.wilber.bootcamplocation_05
 
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(),
     OnMapReadyCallback,
@@ -22,9 +26,13 @@ class MapsActivity : AppCompatActivity(),
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
 
     companion object{
         private const val  LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val  REQUEST_CHECK_SETTINGS = 2
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +44,16 @@ class MapsActivity : AppCompatActivity(),
         mapFragment.getMapAsync(this)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+
+                lastLocation = p0.lastLocation
+                placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+            }
+        }
+        createLocationRequest()
     }
 
     /**
@@ -60,7 +78,7 @@ class MapsActivity : AppCompatActivity(),
         //map.addMarker(MarkerOptions().position(myPlace).title("Minha cidade favorita"))
         //map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12.0f))
 
-        map.getUiSettings().isZoomControlsEnabled(true)
+        map.getUiSettings().isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
 
         setUpMap()
@@ -76,17 +94,95 @@ class MapsActivity : AppCompatActivity(),
 
         //achar localizçaão do usuario
         map.isMyLocationEnabled = true
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
         fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
             if(location != null){
                 lastLocation = location
                 val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12.0f))
             }
         }
     }
 
 
+    private fun placeMarkerOnMap(location: LatLng){
+        val markerOptions = MarkerOptions().position(location)
 
+        //markerOptions.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(resources, R.mipmap.ic_user_location)       ))
+        val titleStr = getAddress(location)
+        markerOptions.title(titleStr)
+
+        map.addMarker(markerOptions)
+    }
+
+    private fun getAddress(latLng: LatLng): String{
+        val geocoder: Geocoder
+        val addresses: List<Address>
+        geocoder = Geocoder(this, Locale.getDefault())
+
+        addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+
+        val address = addresses[0].getAddressLine(0)
+        val city = addresses[0].locality
+        val state = addresses[0].adminArea
+        val country = addresses[0].countryName
+        val postalCode = addresses[0].postalCode
+        return  address
+
+    }
+
+    private fun startLocationUpdates(){
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun createLocationRequest(){
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000  //1 segundo
+        locationRequest.fastestInterval = 5000  //0.5 segundo
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //prioridade alta
+
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdates()
+        }
+        task.addOnFailureListener { e ->
+            if(e is ResolvableApiException){
+                try{
+                    e.startResolutionForResult(this@MapsActivity,
+                    REQUEST_CHECK_SETTINGS)
+                }catch(sendEx: IntentSender.SendIntentException){}
+            }
+
+        }
+    }
+
+    //por causa do consumo de bateria
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(!locationUpdateState){
+            startLocationUpdates()
+        }
+    }
 
     override fun onMarkerClick(p0: Marker?) = false
 }
