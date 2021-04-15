0bbd73969dacab2a5d2520a7abcbb6700654458e
Wilber Peres <42147233+wilberp@users.noreply.github.com>
2021-04-14 21:32:55 -0300

    final serviço assincrono

--- androidStudio/android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
+++ androidStudio/android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/AstrosRepository.kt
@@ -1,10 +1,25 @@
 package com.everis.bootcamp.threading
 
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
-//TODO: 010 - Criar a classe responsável por carregar os dados
 class AstrosRepository {
 
-    //TODO: 011 - Criar função para carregar os astronautas
+    fun loadData() : List<AstrosPeople> {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("http://api.open-notify.org/astros.json")
+            .build()
+
+        val response = client.newCall(request).execute()
+        val result = parserJsonToResult(response.body?.string())
+        return result.people
+    }
+
 
     //TODO: 012 - Criar função para converter o json
+
+    fun parserJsonToResult(json : String?) =
+        Gson().fromJson(json, AstrosResult::class.java)
 }
--- androidStudio/android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/MainActivity.kt
+++ androidStudio/android-bootcamp-threading-exercise-asynctask/app/src/main/java/com/everis/bootcamp/threading/MainActivity.kt
@@ -14,21 +14,60 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //TODO: 018 - fazer o handle do clique do botão
+        button_load_data.setOnClickListener {
+            launchAstroTask()
+        }
     }
 
 
     //TODO: 013 - Criar função para exibir os dados carregados
-
+    fun showData(list: List<AstrosPeople>?){
+        textview_data.text = ""
+        list?.forEach { people ->
+            textview_data.append("${people.name} - ${people.craft} \n\n")
+        }
+    }
 
     //TODO: 014 - Criar função para exibir a ProgressBar
-
+    fun showLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.VISIBLE
+    }
 
     //TODO: 015 - Criar função para esconder a ProgressBar
-
+    fun hideLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.GONE
+    }
 
     //TODO: 017 - Criar função para lançar a Task
+    fun launchAstroTask(){
+        val task = TaskAstros()
+        task.execute()
+    }
 
 
     //TODO: 016 - Criar classe interna para rodar a tarefa assincrona
+    inner class TaskAstros() : AsyncTask<Void, Int, List<AstrosPeople>>(){
+        private val repository = AstrosRepository()
 
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showLoadingIndicator()
+        }
+
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+        }
+
+        override fun doInBackground(vararg params: Void?): List<AstrosPeople> {
+            //onProgressUpdate()
+            return repository.loadData()
+        }
+
+        override fun onPostExecute(result: List<AstrosPeople>?) {
+            super.onPostExecute(result)
+            hideLoadingIndicator()
+            showData(result)
+        }
+    }
 }
